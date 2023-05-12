fb9afc585037d79d7c88484096c5003ab8ab0761
https://github.com/JavierGiberg <gibergjavier@gmail.com>
2023-05-12 15:34:37 +0300

    120523_1

---
+++ src/ass3_linux/HomeFrame.java
@@ -0,0 +1,360 @@
+package ass3_linux;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.concurrent.Semaphore;
+
+public class HomeFrame {
+	private static JTextArea runTextArea;
+	private static JTextArea deadlockTextArea;
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(HomeFrame::createAndShowGUI);
+	}
+
+	private static void createAndShowGUI() {
+
+        JFrame frame = new JFrame("Process Simulation");
+        frame.setSize(800, 800);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        frame.getContentPane().add(mainPanel);
+
+        runTextArea = new JTextArea();
+        runTextArea.setEditable(false);
+
+        JScrollPane runScrollPane = new JScrollPane(runTextArea);
+        runScrollPane.setBorder(BorderFactory.createTitledBorder("Running Processes"));
+
+        deadlockTextArea = new JTextArea();
+        deadlockTextArea.setEditable(false);
+
+        JScrollPane deadlockScrollPane = new JScrollPane(deadlockTextArea);
+        deadlockScrollPane.setBorder(BorderFactory.createTitledBorder("Deadlocked Processes"));
+
+        JPanel processPanel = new JPanel(new GridLayout(1, 2));
+        processPanel.add(runScrollPane);
+        processPanel.add(deadlockScrollPane);
+
+        mainPanel.add(processPanel, BorderLayout.CENTER);
+
+        frame.pack();
+        frame.setSize(650, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        Semaphore semaphore = new Semaphore(0, true);
+        HashMap<Integer, String> runList = new HashMap<>();
+        HashMap<Integer, String> deadlockList = new HashMap<>();
+
+        /*
+         * Install params with I/O
+         */
+        char character = 'A';
+
+        int[] createAvailable = { 0, 0, 0, 0, 0 };
+        for (int i = 0; i < createAvailable.length; i++) {
+            String input = JOptionPane.showInputDialog(frame, "Enter Available [" + character + "]");
+            createAvailable[i] = Integer.parseInt(input);
+            character++;
+        }
+        Available available = new Available(createAvailable);
+
+        String processCountInput = JOptionPane.showInputDialog(frame, "Enter the number of processes:");
+        int processCount = Integer.parseInt(processCountInput);
+        P[] processTable = new P[processCount];
+
+        for (int i = 0; i < processTable.length; i++) {
+            JOptionPane.showMessageDialog(frame, "Handle [P" + (i + 1) + "] Matrices");
+
+            character = 'A';
+            int[] allocation = { 0, 0, 0, 0, 0 };
+            int[] max = { 0, 0, 0, 0, 0 };
+            for (int j = 0; j < allocation.length; j++) {
+                String allocationInput = JOptionPane.showInputDialog(frame, "Enter Allocation [" + character + "]:");
+                allocation[j] = Integer.parseInt(allocationInput);
+
+                String maxInput = JOptionPane.showInputDialog(frame, "Enter Max [" + character + "]:");
+                max[j] = Integer.parseInt(maxInput);
+
+                character++;
+            }
+            processTable[i] = new P(available, max, allocation, semaphore, runList, deadlockList);
+        }
+
+        semaphore.release(1);
+        for (P process : processTable)
+            new Thread(process).start();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        SwingUtilities.invokeLater(() -> {
+            StringBuilder runText = new StringBuilder("Run Process <");
+            for (String process : runList.values()) {
+                runText.append(" ").append(process).append(" ,");
+            }
+            runText.append(">");
+            runTextArea.setText(runText.toString());
+
+            StringBuilder deadlockText = new StringBuilder("Deadlock Process <");
+            for (String process : deadlockList.values()) {
+                deadlockText.append(" ").append(process).append(" ,");
+            }
+            deadlockText.append(">");
+            deadlockTextArea.setText(deadlockText.toString());
+        });
+    }
+
+
+	static class P implements Runnable {
+		private static int count = 1;
+		private static int countLoop = 0;
+		private static int raceCondition = 0;
+		private int id;
+		private int[] need;
+		private Available available;
+		private int[] max;
+		private int[] allocation;
+		Semaphore semaphore;
+		HashMap<Integer, String> runList;
+		HashMap<Integer, String> deadlockList;
+
+		public P(Available available, int[] max, int[] allocation, Semaphore semaphore,
+				HashMap<Integer, String> runList, HashMap<Integer, String> deadlockList) {
+			this.id = count;
+			count++;
+			this.allocation = allocation;
+			this.max = max;
+			this.available = available;
+			this.need = needTableHandle();
+			this.semaphore = semaphore;
+			this.runList = runList;
+			this.deadlockList = deadlockList;
+		}
+
+		public void run() {
+			int equalFlag = 0;
+
+			while (countLoop <= 4) {
+				try {
+					semaphore.acquire();
+					System.out.println("countLoop " + countLoop + " P[" + id + "]");
+					try {
+						Thread.sleep(250);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					for (int i = 0; i < need.length; i++) {
+						if (need[i] <= available.getAvailable(i)) {
+							equalFlag++;
+						} else {
+							equalFlag = 0;
+							break;
+						}
+					}
+					if (equalFlag == need.length) {
+						countLoop = 0;
+						runList.put(raceCondition, "[P" + id + "]");
+						deadlockList.remove(id);
+						raceCondition++;
+						for (int i = 0; i < need.length; i++) {
+							available.setAvailable(i, available.getAvailable(i) + allocation[i]);
+						}
+						break;
+					} else {
+						if (!deadlockList.containsKey(id))
+							deadlockList.put(id, "[P" + id + "]");
+						countLoop++;
+						semaphore.release();
+					}
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			System.out.println("P[" + id + "] DONE");
+			for (int i = 0; i < available.getAvailableSize(); i++)
+				System.out.println(available.getAvailable(i));
+			semaphore.release();
+		}
+
+		private int[] needTableHandle() {
+			int[] makeTable = new int[available.getAvailableSize()];
+			for (int i = 0; i < makeTable.length; i++) {
+				makeTable[i] = this.max[i] - this.allocation[i];
+			}
+			return makeTable;
+		}
+
+		public int getNeed(int i) {
+			return need[i];
+		}
+
+		public int getMax(int i) {
+			return max[i];
+		}
+
+		public int getAllocation(int i) {
+			return allocation[i];
+		}
+
+		public void setAllocation(int i, int value) {
+			allocation[i] = value;
+		}
+
+		public int getId() {
+			return id;
+		}
+	}
+
+	static class Available {
+		private int[] available;
+
+		public Available(int[] available) {
+			this.available = available;
+		}
+
+		public synchronized void setAvailable(int i, int value) {
+			available[i] = value;
+		}
+
+		public synchronized int getAvailable(int i) {
+			return available[i];
+		}
+
+		public int getAvailableSize() {
+			return available.length;
+
+		}
+	}
+}
+//==============================================================================
+//
+//import javax.swing.*;
+//
+//import ass3_linux.main.Available;
+//import ass3_linux.main.P;
+//
+//import java.awt.*;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
+//import java.util.HashMap;
+//import java.util.concurrent.Semaphore;
+//
+//public class HomeFrame {
+//    private JFrame frame;
+//    private JTextField[] availableFields;
+//    private JTextField numOfProcessesField;
+//    private JTextArea outputArea;
+//
+//    public HomeFrame() {
+//        initialize();
+//    }
+//
+//    private void initialize() {
+//        frame = new JFrame();
+//        frame.setTitle("Swing Integration");
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.setSize(400, 300);
+//        frame.setLocationRelativeTo(null);
+//
+//        JPanel mainPanel = new JPanel();
+//        mainPanel.setLayout(new BorderLayout());
+//
+//        JPanel inputPanel = new JPanel();
+//        inputPanel.setLayout(new FlowLayout());
+//
+//        availableFields = new JTextField[5];
+//        for (int i = 0; i < 5; i++) {
+//            availableFields[i] = new JTextField(5);
+//            inputPanel.add(availableFields[i]);
+//        }
+//
+//        numOfProcessesField = new JTextField(5);
+//        inputPanel.add(numOfProcessesField);
+//
+//        JButton runButton = new JButton("Run");
+//        runButton.addActionListener(new ActionListener() {
+//            public void actionPerformed(ActionEvent e) {
+//                runAlgorithm();
+//            }
+//        });
+//        inputPanel.add(runButton);
+//
+//        mainPanel.add(inputPanel, BorderLayout.NORTH);
+//
+//        outputArea = new JTextArea();
+//        outputArea.setEditable(false);
+//        JScrollPane scrollPane = new JScrollPane(outputArea);
+//        mainPanel.add(scrollPane, BorderLayout.CENTER);
+//
+//        frame.getContentPane().add(mainPanel);
+//    }
+//
+//    private void runAlgorithm() {
+//        Semaphore semaphore = new Semaphore(0, true);
+//        HashMap<Integer, String> runList = new HashMap<>();
+//        HashMap<Integer, String> deadlockList = new HashMap<>();
+//
+//        int[] createAvailable = new int[5];
+//        for (int i = 0; i < availableFields.length; i++) {
+//            createAvailable[i] = Integer.parseInt(availableFields[i].getText());
+//        }
+//
+//        Available available = new Available(createAvailable);
+//        int numOfProcesses = Integer.parseInt(numOfProcessesField.getText());
+//        P[] processTable = new P[numOfProcesses];
+//
+//        for (int i = 0; i < processTable.length; i++) {
+//            int[] allocation = new int[5];
+//            int[] max = new int[5];
+//            // Initialize allocation and max arrays according to your requirements
+//            processTable[i] = new P(available, max, allocation, semaphore, runList, deadlockList);
+//        }
+//
+//        semaphore.release(1);
+//
+//        for (P process : processTable) {
+//            new Thread(process).start();
+//        }
+//
+//        try {
+//            Thread.sleep(15000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//
+//        StringBuilder runProcessOutput = new StringBuilder("Run Process: ");
+//        for (String process : runList.values()) {
+//            runProcessOutput.append(process).append(", ");
+//        }
+//
+//        StringBuilder deadlockProcessOutput = new StringBuilder("Deadlock Process: ");
+//        for (Integer process : deadlockList.keySet()) {
+//            deadlockProcessOutput.append(process).append(", ");
+//        }
+//
+//        outputArea.setText(runProcessOutput.toString() + "\n" + deadlockProcessOutput.toString());
+//    }
+//
+//    public void show() {
+//        frame.setVisible(true);
+//    }
+//
+//    public static void main(String[] args) {
+//    	HomeFrame example = new HomeFrame();
+//        example.show();
+//    }
+//}
+//
+//
+//
+//
---
+++ src/ass3_linux/main.java
@@ -0,0 +1,210 @@
+package ass3_linux;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.concurrent.Semaphore;
+
+public class main {
+
+	public static void main(String[] args) {
+		Semaphore semaphore = new Semaphore(0, true);
+		HashMap<Integer, String> RunList = new HashMap<>();
+		HashMap<Integer, String> DeadlockList = new HashMap<>();
+		/*
+		 * install params with I/O
+		 */
+		char character = 'A';
+		Scanner sc = new Scanner(System.in);
+		int[] createAvailable = { 0, 0, 0, 0, 0 };
+		System.out.println("Enter Available array: ");
+		for (int i = 0; i < createAvailable.length; i++) {
+			System.out.println("Enter Available [" + character + "] ");
+			createAvailable[i] = sc.nextInt();
+			character++;
+		}
+		Available available = new Available(createAvailable);
+		System.out.println("Enter num of process: ");
+		P[] processTable = new P[sc.nextInt()];
+
+		for (int i = 0; i < processTable.length; i++) {
+			System.out.println("Handle [P" + (i + 1) + "] Matixs'");
+			character = 'A';
+			int[] Allocation = { 0, 0, 0, 0, 0 };
+			int[] Max = { 0, 0, 0, 0, 0 };
+			for (int j = 0; j < Allocation.length; j++) {
+				System.out.println("Enter Allocation [" + character + "] :");
+				Allocation[j] = sc.nextInt();
+				System.out.println("Enter Max [" + character + "] :");
+				Max[j] = sc.nextInt();
+				character++;
+			}
+			processTable[i] = new P(available, Max, Allocation, semaphore, RunList, DeadlockList);
+			;
+		}
+		semaphore.release(1);
+		for (P process : processTable)
+			new Thread(process).start();
+		try {
+			Thread.currentThread().sleep(15000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.print("Run Process <");
+		for (int i = 0; i < RunList.size(); i++) {
+			System.out.print(" " + RunList.get(i) + " ,");
+		}
+		System.out.print(">");
+		System.out.println();
+		System.out.println("---------------------------");
+		System.out.print("Deadlock Process <");
+		for (Integer item : DeadlockList.keySet()) {
+			System.out.print(" " + item + " ,");
+		}
+		System.out.print(" >");
+
+	}
+	/*
+	 * classe P (Process)
+	 */
+
+	static class P implements Runnable {
+		private static int count = 1;
+		private static int countLoop = 0;
+		private static int raceCondition = 0;
+		private int id;;
+		private int[] Need;
+		private Available available;
+		private int[] Max;
+		private int[] Allocation;
+		Semaphore semaphore;
+		HashMap<Integer, String> RunList;
+		HashMap<Integer, String> DeadlockList;
+
+		public P(Available available, int[] Max, int[] Allocation, Semaphore semaphore,
+				HashMap<Integer, String> RunList, HashMap<Integer, String> DeadlockList) {
+			this.id = count;
+			count++;
+			this.Allocation = Allocation;
+			this.Max = Max;
+			this.available = available;
+			this.Need = needTableHandle();
+			this.semaphore = semaphore;
+			this.RunList = RunList;
+			this.DeadlockList = DeadlockList;
+		}
+
+		// -----------------------Run block------------------------------------------
+		public void run() {
+			int equalFlag = 0;
+
+			while (countLoop<=4) {
+				try {
+					semaphore.acquire();
+					System.out.println("countLoop " + countLoop + " P[" + id + "]");
+					try {
+						Thread.currentThread().sleep(250);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					for (int i = 0; i < Need.length; i++) {
+						if (Need[i] <= available.getAvailable(i)) {
+							equalFlag++;
+						} else {
+							equalFlag = 0;
+							break;
+						}
+					}
+					if (equalFlag == Need.length) {
+						countLoop = 0;
+						RunList.put(raceCondition, "[P" + id + "]");
+						DeadlockList.remove(id);
+						raceCondition++;
+						for (int i = 0; i < Need.length; i++) {
+							available.setAvailable(i, available.getAvailable(i) + Allocation[i]);
+						}
+
+						break;
+					} else {
+						if (!DeadlockList.containsKey(id))
+							DeadlockList.put(id, "[P" + id + "]");
+						countLoop++;
+						semaphore.release();
+					}
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+//				
+			}
+			System.out.println("P[" + id + "] DONE");
+			for (int i = 0; i < available.getAvailableSize(); i++)
+				System.out.println(available.getAvailable(i));
+			semaphore.release();
+
+		}
+
+		// -----------------------Run block------------------------------------------
+		/*
+		 * make Need Table
+		 */
+		private int[] needTableHandle() {
+			int[] makeTable = new int[available.getAvailableSize()];
+			for (int i = 0; i < makeTable.length; i++) {
+				makeTable[i] = this.Max[i] - this.Allocation[i];
+			}
+			return makeTable;
+		}
+
+		public int getNeed(int i) {
+			return Need[i];
+		}
+
+		public int getMax(int i) {
+			return Max[i];
+		}
+
+		public int getAllocation(int i) {
+			return Allocation[i];
+		}
+
+		public void setAllocation(int i, int value) {
+			Allocation[i] = value;
+		}
+
+		public int getId() {
+			return id;
+		}
+
+	}
+	/*
+	 * class Available ( share resource )
+	 */
+
+	static class Available {
+		private int[] available;
+
+		public Available(int[] available) {
+			this.available = available;
+		}
+
+		public synchronized void setAvailable(int i, int value) {
+			available[i] = value;
+		}
+
+		public synchronized int getAvailable(int i) {
+			return available[i];
+		}
+
+//		public synchronized void setAvailable(int[] available) {
+//			this.available = available;
+//		}
+
+		public int getAvailableSize() {
+			return available.length;
+		}
+
+	}
+
+}
